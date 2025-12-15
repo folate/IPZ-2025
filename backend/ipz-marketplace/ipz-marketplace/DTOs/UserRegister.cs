184631b83cfa2a10d6d23ae2bd5b8a55ae13a68d
Pacyn <erykpacyna210@gmail.com>
2025-12-15 18:16:53 +0100

    I2-97
    schema for Login and Register user

---
+++ backend/ipz-marketplace/ipz-marketplace/DTOs/UserRegister.cs
@@ -0,0 +1,17 @@
+﻿namespace ipz_marketplace.DTOs
+{
+    public class UserRegister
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
