95af65d7a58f4e962f70427127aa743cd560a3cf
folate <52334543+folate@users.noreply.github.com>
2026-01-03 01:33:57 +0100

    apply migrations on startup

--- backend/ipz-marketplace/ipz-marketplace/Program.cs
+++ backend/ipz-marketplace/ipz-marketplace/Program.cs
@@ -28,7 +28,19 @@ public class Program
         using (var scope = app.Services.CreateScope())
         {
             var services = scope.ServiceProvider;
-            await DatabaseSeeder.SeedAsync(services, app.Environment);
+            try 
+            {
+                var context = services.GetRequiredService<MarketplaceDbContext>();
+        
+                await context.Database.MigrateAsync();
+
+                await DatabaseSeeder.SeedAsync(services, app.Environment);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration or seeding.");
+            }
         }
 
         // Configure the HTTP request pipeline.
