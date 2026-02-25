b274cbf2101d9fe5e2f7bed45ada193d649cf65a
Pacyn <erykpacyna210@gmail.com>
2026-02-25 18:16:52 +0100

    search and email(kinda works)

---
+++ backend/ipz-marketplace/ipz-marketplace/Controllers/EmailController.cs
@@ -0,0 +1,44 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ipz_marketplace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
+    {
+        public EmailController()
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail()
+        {
+            var email = new MimeMessage();
+            var smtpClient = new SmtpClient();
+            var builder = new BodyBuilder()
+            {
+                TextBody = @"This is a test email sent from the IPZ Marketplace application."
+            };
+            try
+            {
+                email.From.Add(new MailboxAddress("IPZ Marketplace", "noreply@ipzmarketplace.com"));
+                email.To.Add(new MailboxAddress("Recipient", "pe55848@zut.edu.pl"));
+                email.Subject = "Test Email from IPZ Marketplace";
+                email.Body = builder.ToMessageBody();
+                smtpClient.Connect("live.smtp.mailtrap.io", 587, false);
+
+                smtpClient.Authenticate("api", "703681c4b6878019963803894ca30d88");
+                smtpClient.Send(email);
+                smtpClient.Disconnect(true);
+            }
+            catch (Exception ex) {
+                return BadRequest($"Failed to send email: {ex.Message}");
+            }
+
+            return Ok("Email sent successfully");
+        }
+    }
+}
---
+++ backend/ipz-marketplace/ipz-marketplace/Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using ipz_marketplace.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ipz_marketplace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly MarketplaceDbContext _context;
+        public SearchController(MarketplaceDbContext context) 
+        { 
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Index([FromQuery] string search)
+        {
+            var content = _context.SellerAds
+                .AsNoTracking()
+                .Select(
+                    a => new ListingSellerAdDTO
+                    {
+                        Id = a.Id,
+                        Title = a.Title,
+                        Description = a.Description,
+                        Freelancer = a.Freelancer.UserName,
+                        Gigs = a.Gigs.Select(g => new GigsDTO
+                        {
+                            Id = g.Id,
+                            TierName = g.TierName,
+                            TierDescription = g.TierDescription,
+                            Price = g.Price
+                        }).ToList()
+                    })
+                .Where(p => EF.Functions.ILike(p.Title, $"%{search}%") || EF.Functions.ILike(p.Description, $"%{search}%"))
+                .Take(10)
+                .ToList();
+            if(content != null)
+                return Ok(content);
+            return BadRequest();
+        }
+    }
+}
